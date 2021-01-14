36369c0dab4ecb79ff1e6efbe982d1d63b3924dc
asmae1m <asmaemahjoubi6@gmail.com>
2021-01-14 17:19:44 +0100

    Lesen ist bereit!

---
+++ Lesen/static/JSFiles/ToutesLesPagesJS/script.js
@@ -0,0 +1,7 @@
+let btnshow =document.querySelection('button');
+let resultat=document.querySelection('h1');
+btnshow.addEventListener('click')
+btwShow.addEventListener('click', () ==> {
+	let checkbox = document.querySelection("input [type ="checkbox"]:checke
+		d
+} );
